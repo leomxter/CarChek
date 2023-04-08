ac37cf5e71291a5506ef96fd498ae7ad4ceb3c7c
Elmyrza Doolotbekov <elmyrza.do@gmail.com>
2023-04-08 16:07:37 +0600

    Cars Models

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    number = models.CharField(
+        max_length=10,
+        verbose_name="Номер"
+
+    )
+    make = models.CharField(
+        max_length=50,
+        verbose_name="Сделано"
+
+        )
+    model = models.CharField(
+        max_length=50,
+        verbose_name="Модель"
+
+        )
+    year = models.CharField(
+        max_length=50,
+        verbose_name="Год"
+    )
+
+    def __str__(self):
+        return self.number
